eafdd285a63c78b0a1c279f8b8b39c73d7651b49
alicefingiro <alicefingiro@localhost>
2026-05-28 19:05:08 -0300

    teste commit deu certo

--- JogoDesenvolvido/src/Personagens/Mago.java
+++ JogoDesenvolvido/src/Personagens/Mago.java
@@ -24,7 +24,6 @@ public class Mago extends Personagem implements Corredor, Voador{
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
-    testeeee acho que deu certo
 }
 
     
